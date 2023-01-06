6f30b3a2aa7f29ca47e2182e4f29f7cc57bc7098
PC-0003\User <xx@gmail.com>
2023-01-06 11:58:33 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1= new Student();
+            stu1.setStudentName("Perapat");
+            //stu1.studentId = "644234021";
+            //stu1.studentName = "Perapat";
+            //stu1.studentAge = 20;
+            //stu1.studentCourse = "Computer Science";
+
+            Student stu2 = new Student("0000001","Somsom","Com",21);
+            //stu2.studentId = "0000001";
+            //stu2.studentName = "Cat";
+            //stu2.studentAge = 2;
+            //stu2.studentCourse = "Computer Science";
+
+            stu1.SaySomething();
+            stu2.SaySomething();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndConstructors
+{
+    internal class Student
+    {
+        String studentId;
+        String studentName;
+        int studentAge;
+        String studentCourse;
+        
+        //Constructor คือ Method ที่ชื่่อเหมือน Class ทำงานเมื่อมีการสร้าง Object ใหม่ของ Class ขึ้นมา
+        public Student()
+        {
+            studentId = "0000000";
+            studentName = "Unknown";
+            studentCourse = "Unknown";
+            studentAge = 0;
+        }
+
+        //Constructor
+        public Student(String id,String name,String course,int age)
+        {
+            studentId = id;
+            studentName = name;
+            studentCourse = course;
+            studentAge = age;
+        }
+        public void setStudentName(String name)
+        {
+            studentName = name;
+        }
+
+        public void SaySomething()
+        {
+            Console.WriteLine("Good Morning, My name is {0}.", studentName);
+            Console.WriteLine("I'm {0} years old. My student ID is {1}.", studentAge, studentId);
+            Console.WriteLine("I'm a {0} student at Songkhla Rajabhat University.", studentCourse);
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
